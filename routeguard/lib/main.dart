101a81f28c2a0644e8e35bacc9cb41ec36ae9818
Meenu2019 <54184349+Meenu2019@users.noreply.github.com>
2024-02-23 16:26:11 +0530

    update location

--- routeguard/lib/main.dart
+++ routeguard/lib/main.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart' as geocoding;
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +9,7 @@ void main() {
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
   
 
   // This widget is the root of your application.
@@ -16,21 +18,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'RouteGuard',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
@@ -41,15 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -62,17 +40,39 @@ class _MyHomePageState extends State<MyHomePage> {
   TextEditingController destinationController = TextEditingController();
   LatLng destinationCoordinates = const LatLng(37.7749, -122.4194);
 
-  void goToDestination(String input) {
-    if (input.isNotEmpty) {
-      setState(() {
-        destinationCoordinates = const LatLng(40.7128, -74.0060);
-      });
+  void goToDestination(String input) async {
+    print('Destination submitted: $input');
+    
+  if (input.isNotEmpty) {
+    try {
+      List<geocoding.Location> locations =
+          await geocoding.locationFromAddress(input);
+      if (locations.isNotEmpty) {
+        geocoding.Location location = locations.first;
+        print('Location found: $location');
+        setState(() {
+          destinationCoordinates =
+              LatLng(location.latitude, location.longitude);
+          print('Destination coordinates updated: $destinationCoordinates');
+        });
 
-      mapController.animateCamera(
-        CameraUpdate.newLatLngZoom(destinationCoordinates, 12.0)
-      );
+        mapController.animateCamera(CameraUpdate.newLatLngZoom(
+            destinationCoordinates, 12.0));
+          print('Map updated successfully');
+      } else {
+        // Handle case where no location was found
+        print('No location found for the input: $input');
+      }
+    } catch (e) {
+      // Handle any errors that might occur during geocoding
+      print('Error geocoding the input: $e');
     }
+  } else {
+    // Handle case where input is empty
+    print('Input is empty');
   }
+}
+
 
   
   Widget buildMap(BuildContext context) {
@@ -89,7 +89,10 @@ class _MyHomePageState extends State<MyHomePage> {
               border: OutlineInputBorder(),
               labelText: 'Enter the destination',
             ),
-            onSubmitted: goToDestination,
+            onSubmitted: (value) {
+              print('Submitted: $value');
+              goToDestination(value);
+            },
           ),
           Expanded(
             child: GoogleMap(
@@ -112,87 +115,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _controller = TextEditingController();
-  // }
-
-  // @override
-  // void dispose() {
-  //   _controller.dispose();
-  //   super.dispose();
-  // }
-
   
   @override
   Widget build(BuildContext context) {
     return buildMap(context);
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     // TRY THIS: Try changing the color here to a specific color (to
-    //     // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-    //     // change color while the other colors stay the same.
-    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    //     // Here we take the value from the MyHomePage object that was created by
-    //     // the App.build method, and use it to set our appbar title.
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     // Center is a layout widget. It takes a single child and positions it
-    //     // in the middle of the parent.
-    //     child: Column(
-    //       // Column is also a layout widget. It takes a list of children and
-    //       // arranges them vertically. By default, it sizes itself to fit its
-    //       // children horizontally, and tries to be as tall as its parent.
-    //       //
-    //       // Column has various properties to control how it sizes itself and
-    //       // how it positions its children. Here we use mainAxisAlignment to
-    //       // center the children vertically; the main axis here is the vertical
-    //       // axis because Columns are vertical (the cross axis would be
-    //       // horizontal).
-    //       //
-    //       // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-    //       // action in the IDE, or press "p" in the console), to see the
-    //       // wireframe for each widget.
-    //       mainAxisAlignment: MainAxisAlignment.start,
-    //       children: <Widget>[
-    //         const TextField(
-    //           decoration: InputDecoration(
-    //             border: OutlineInputBorder(),
-    //             labelText: 'Enter the destination',
-    //           ),
-    //           // controller: _controller,
-    //           // onSubmitted: (
-                
-    //           // ) {}
-    //         ),
-    //         TextButton(
-    //           style: TextButton.styleFrom(
-    //             textStyle: const TextStyle(fontSize: 25)
-    //           ),
-    //           child: const Align(
-    //             alignment: Alignment.center,
-    //             child: Text(
-    //               "Start Journey"
-
-    //           )
-    //           ),
-    //           onPressed: () {},
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //         // This trailing comma makes auto-formatting nicer for build methods.
-    // );
   }
 }
-
-
